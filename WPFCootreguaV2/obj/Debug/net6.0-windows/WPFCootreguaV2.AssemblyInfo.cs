b27d7148883bea82abaa14cabd84379685b99294
anacprieto <aprieto@e-city.co>
2025-05-05 10:31:16 -0500

    Cambios de mi list

--- WPFCootreguaV2/obj/Debug/net6.0-windows/WPFCootreguaV2.AssemblyInfo.cs
+++ WPFCootreguaV2/obj/Debug/net6.0-windows/WPFCootreguaV2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WPFCootreguaV2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+258800aafcb771d74b426682190e53471975b421")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f39869ac0ea34203f8d8c59c0934e526fedb356a")]
 [assembly: System.Reflection.AssemblyProductAttribute("WPFCootreguaV2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WPFCootreguaV2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

8556f73ead1e596008c79521e2e199d8fb07be6e
anacprieto <aprieto@e-city.co>
2025-05-06 17:18:44 -0500

    Estable

--- WPFCootreguaV2/obj/Debug/net6.0-windows/WPFCootreguaV2.AssemblyInfo.cs
+++ WPFCootreguaV2/obj/Debug/net6.0-windows/WPFCootreguaV2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WPFCootreguaV2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc5ecf3b3b7136fe52f2e2624c2afc2e30079c25")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e9fbeb9a0d4ce72afb34aea4fb1f6d2549d603c")]
 [assembly: System.Reflection.AssemblyProductAttribute("WPFCootreguaV2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WPFCootreguaV2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
